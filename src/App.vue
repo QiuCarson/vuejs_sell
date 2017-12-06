1958221b1bff80af241f2539a9de209edd76fbda
裘益松 <qiuyisong@gmail.com>
2017-12-06 22:55:32 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab broder-1px">
       <div class="tab-item"><router-link to="/goods" active-class>商品</router-link></div>
       <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
       <div class="tab-item"><router-link to="/seller">商家</router-link></div>
@@ -19,13 +19,15 @@
   };
 </script>
 
-<style lang="stylus" ref="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixin.styl';
   #app
     .tab
       display:flex
       width:100%
       height:40px
       line-height :40px
+      border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex:1
         text-align: center
@@ -35,6 +37,5 @@
           color: rgb(77,85,93)
           &.active
             color: rgb(240,20,20)
-
 
 </style>

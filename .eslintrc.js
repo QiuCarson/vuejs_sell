e1397f4bca25b860b9281ccaa3f3758a6c52f522
qiuyisong <qiuyisong@gmail.com>
2017-12-07 17:37:00 +0800

    app

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     //不带分号报错误
     semi:['error','always'],
-    'indent': 0
+    'indent': 0,
+    'space-before-function-paren': 0
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab broder-1px">
-      <div class="tab-item"><router-link to="/goods" active-class>商品</router-link></div>
-      <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
-      <div class="tab-item"><router-link to="/seller">商家</router-link></div>
+      <div class="tab-item">
+        <router-link to="/goods" active-class>商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
     </div>
     <router-view></router-view>
   </div>
@@ -12,7 +18,23 @@
 
 <script type="text/ecmascript-6">
   import header from './components/header/header.vue';
+
+  const ERR_OK = 0;
+
   export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.seller = response.data;
+        }
+      });
+    },
     components: {
       'v-header': header
     }
@@ -23,19 +45,19 @@
   @import './common/stylus/mixin.styl';
   #app
     .tab
-      display:flex
-      width:100%
-      height:40px
-      line-height :40px
-      border-1px(rgba(7,17,27,0.1))
+      display: flex
+      width: 100%
+      height: 40px
+      line-height: 40px
+      border-1px(rgba(7, 17, 27, 0.1))
       .tab-item
-        flex:1
+        flex: 1
         text-align: center
         & > a
           display: block
           font-size: 14px
-          color: rgb(77,85,93)
+          color: rgb(77, 85, 93)
           &.active
-            color: rgb(240,20,20)
+            color: rgb(240, 20, 20)
 
 </style>
